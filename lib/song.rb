4ddab7ff91aa4fcfc25aaf9ce4d7fbc15b86c472
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-22 15:15:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
 
-  attr_accessor :artist, :name, :genres
+  attr_accessor :artist, :name, :genre
 
   def initialize(name, genre)
     @name = name
     @genres = genre
     genre.songs<<self
   end
+
+
 
 end
